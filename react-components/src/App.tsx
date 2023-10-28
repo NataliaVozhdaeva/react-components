3fa8d54452f4cb2fea82c320677e064f2aba03aa
NataliaVozhdaeva <LomzNat@yandex.ru>
2023-10-29 00:02:34 +0200

    test: precommit

--- react-components/src/App.tsx
+++ react-components/src/App.tsx
@@ -3,8 +3,8 @@ import './App.css';
 function App() {
   return (
     <>
-      <div>Hello world!</div>
-      <input id="dinosour-name"   className="dinosour-input"    type="text"       data-text="this is my dinosour"
+      <div>Hello world!</div>    
+      <input id="dinosour-name"      className="dinosour-input"    type="text"       data-text="this is my dinosour"
       /> 
     </>
   );
