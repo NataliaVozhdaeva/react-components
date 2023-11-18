ed6d039e37730ce54690e1dfb03b6a50f3546459
NataliaVozhdaeva <LomzNat@yandex.ru>
2023-11-18 20:23:13 +0200

    fix: access to abilities

--- src/pages/pokemon-details.tsx
+++ src/pages/pokemon-details.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import { Pokemon } from '../services/interfaces';
+import { Pokemon, Abilities } from '../services/interfaces';
 import { useParams } from 'react-router-dom';
 import { useGetPokemonByNameQuery } from '../services/api';
 
@@ -9,18 +9,10 @@ const PokemonDetails = () => {
   const { data, isLoading } = useGetPokemonByNameQuery(params.name);
   let img: string;
   if (data) {
-    console.log('poke ', data.sprites.other.dream_world.front_default);
     img = data.sprites.other.dream_world.front_default;
   }
-  /*  useEffect(() => {
-    if(data){
-    renderDetais(data)}
-  }, []); */
-  /* if(!data) throw new Error('data')
-  const abilities: Abilities[] = data.abilities; */
 
   const renderDetais = (data: Pokemon) => {
-    //console.log('poke ', data)
     return (
       <>
         <Link to="/">
@@ -30,15 +22,15 @@ const PokemonDetails = () => {
         <img data-testid="details-img" src={img} width={300} />
         <div data-testid="details-abilities" className="abilities">
           <h4>Abilities: </h4>
-          {/* <ul className="abilities-list">
-            {abilities.map((el) => {
+          <ul className="abilities-list">
+            {data.abilities.map((el: Abilities) => {
               return (
                 <li className="item" key={el.ability.name}>
                   {el.ability.name}{' '}
                 </li>
               );
             })}
-          </ul> */}
+          </ul>
         </div>
       </>
     );
