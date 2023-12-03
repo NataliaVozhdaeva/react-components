6f2a5a67bb4352176404bd1785f4f78a46603b3f
NataliaVozhdaeva <LomzNat@yandex.ru>
2023-12-03 22:31:51 +0400

    init: react-forms project

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,17 @@
 import { Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store/store';
-import { Header } from './components/header/header';
 import { MainPage } from './pages/main-page';
-import { PokemonDetails } from './pages/pokemon-details';
-import { NotFoundPage } from './pages/not-found';
-import { Pagination } from './components/main/pagination';
 
 export function App(): JSX.Element {
   return (
     <Provider store={store}>
-      <Header />
       <Routes>
         <Route path="/" element={<MainPage />}>
-          <Route path="/:name" element={<PokemonDetails />} />
+          <Route path="/withreactform" />
+          <Route path="/withoutreactform" />
         </Route>
-        <Route path="*" element={<NotFoundPage />} />
       </Routes>
-      <Pagination />
     </Provider>
   );
 }
--- src/services/constants.tsx
+++
@@ -1,3 +0,0 @@
-const API_PATH = 'https://pokeapi.co/api/v2/pokemon/';
-
-export default API_PATH;
