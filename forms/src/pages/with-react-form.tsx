ac1112aa3937acb6a8e94cc36ae50ba264e9afdf
NataliaVozhdaeva <LomzNat@yandex.ru>
2023-12-05 00:06:24 +0400

    feat: add name- and email-field and validation

--- forms/src/pages/with-react-form.tsx
+++ forms/src/pages/with-react-form.tsx
@@ -2,7 +2,11 @@ import { SubmitHandler, useForm } from 'react-hook-form';
 import IFormFields from '../intefaces';
 
 const WithReactForm = () => {
-  const { register, handleSubmit } = useForm<IFormFields>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormFields>();
   const onSubmit: SubmitHandler<IFormFields> = (data) => {
     console.log(data.name, data.age, data.checkbox);
   };
@@ -11,13 +15,43 @@ const WithReactForm = () => {
     <div className="main-wrapper">
       <h2 className="title">With React-Hook-Form</h2>
       <form action="" onSubmit={handleSubmit(onSubmit)}>
-        <input
-          {...(register('name'), { required: true })}
-          type="text"
-          name="name"
-          id="name"
-          className="input"
-        />
+        <label htmlFor="name" className="field-container">
+          {' '}
+          Name
+          <input
+            {...register('name', {
+              required: 'Name is required',
+              pattern: {
+                message: 'Name should start from the capital letter',
+                value: /^[A-Z]{1}[a-z]*$/,
+              },
+            })}
+            type="text"
+            name="name"
+            id="name"
+            className="input"
+          />
+          {errors?.name && <div className="error">{errors.name.message}</div>}
+        </label>
+
+        <label htmlFor="email" className="field-container">
+          {' '}
+          E-mail
+          <input
+            {...register('email', {
+              pattern: {
+                message: 'Please, enter a valid email',
+                value:
+                  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+              },
+            })}
+            type="text"
+            name="email"
+            id="email"
+            className="input"
+          />
+          {errors?.email && <div className="error">{errors.email.message}</div>}
+        </label>
         <button type="submit" className="btn">
           Submit
         </button>
